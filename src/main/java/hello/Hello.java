49c3bcefcb30e7a2284712f7ab37c765293577f2
Jim Crossley <jim@crossleys.org>
2018-09-07 16:26:42 -0400

    A more fn-ish function

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -1,17 +1,11 @@
 package hello;
 
-import com.google.gson.JsonObject;
-
 public class Hello {
-    public static JsonObject main(JsonObject args) {
-        String name = "stranger";
-        if (args.has("name"))
-            name = args.getAsJsonPrimitive("name").getAsString();
-        String greeting = "Hello " + name + "!";
-        System.out.println(greeting);
-        System.err.println("Java to stderr");
-        JsonObject response = new JsonObject();
-        response.addProperty("greeting", greeting);
-        return response;
+
+    public String handleRequest(String input) {
+        String name = (input == null || input.isEmpty()) ? "world"  : input;
+
+        return "Hello, " + name + "!";
     }
+
 }
